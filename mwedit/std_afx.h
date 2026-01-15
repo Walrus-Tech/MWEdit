8ec846cd0397d8628f8343f9898ccd48659e8fdb
deaths_soul <walrusramblings@gmail.com>
2026-01-15 04:11:00 -0500

    Restore MFC support

--- mwedit/std_afx.h
+++ mwedit/std_afx.h
@@ -5,19 +5,19 @@
 #ifndef AFX_STDAFX_H__AB04E5C5_2483_420A_AA8E_0DC2590FE532__INCLUDED_
 #define AFX_STDAFX_H__AB04E5C5_2483_420A_AA8E_0DC2590FE532__INCLUDED_
 
-//#if _MSC_VER > 1000
-	//#pragma once
-//#endif // _MSC_VER > 1000
+#if _MSC_VER > 1000
+	#pragma once
+#endif // _MSC_VER > 1000
 
-//#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
 
-//#include <afxwin.h>             // MFC core and standard components
-//#include <afxext.h>             // MFC extensions
-//#include <afxdisp.h>            // MFC Automation classes
-//#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
-//#ifndef _AFX_NO_AFXCMN_SUPPORT
-	//#include <afxcmn.h>         // MFC support for Windows Common Controls
-//#endif // _AFX_NO_AFXCMN_SUPPORT
+#include <afxwin.h>             // MFC core and standard components
+#include <afxext.h>             // MFC extensions
+#include <afxdisp.h>            // MFC Automation classes
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+	#include <afxcmn.h>         // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
 
 //#pragma comment(lib, "WINMM.lib")
 //#pragma comment(lib, "../IL/DevIL.lib")
